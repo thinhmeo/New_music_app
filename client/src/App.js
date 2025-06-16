89a517847f03d82faf59620d450e9884f898d081
kendunothingelse <21130399@st.hcmuaf.edu.vn>
2025-06-16 21:37:08 +0700

    them chinh sua profile

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import React, { useEffect, useState } from 'react'
-import { Route, Routes, useNavigate, useLocation } from 'react-router-dom'
-import { Dashboard, Music, Home, Login, MusicPlayer } from './components'
+import React, {useEffect, useState} from 'react'
+import {Route, Routes, useNavigate, useLocation} from 'react-router-dom'
+import {Dashboard, Music, Home, Login, MusicPlayer} from './components'
 import MyFavorite from './components/MyFavorite/MyFavorite';
 
-import { app } from './config/firebase.config'
+import {app} from './config/firebase.config'
 
-import { getAuth } from 'firebase/auth'
+import {getAuth} from 'firebase/auth'
 
-import { AnimatePresence, motion } from 'framer-motion'
-import { validateUser } from './api'
-import { useStateValue } from './context/StateProvider'
-import { actionType } from './context/reducer'
+import {AnimatePresence, motion} from 'framer-motion'
+import {validateUser} from './api'
+import {useStateValue} from './context/StateProvider'
+import {actionType} from './context/reducer'
 import MainLayout from "./components/MainLayout";
+import Register from "./components/login/Register";
+import UserProfile from "./components/profile/UserProfile";
 
 const App = () => {
     const firebaseAuth = getAuth(app);
     const navigate = useNavigate();
     const location = useLocation();
 
-    const [{ user, isSongPlaying }, dispatch] = useStateValue();
+    const [{user, isSongPlaying}, dispatch] = useStateValue();
 
     const [auth, setAuth] = useState(false || window.localStorage.getItem("auth") === "true");
 
@@ -53,28 +55,32 @@ const App = () => {
                     {/* Layout có header, không áp dụng cho /login */}
                     {/* Hiển thị header nếu không phải trang login */}
                     {location.pathname !== "/login" ? (
-                        <Route element={<MainLayout />}>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/musics" element={<Music />} />
-                            <Route path="/favorites" element={<MyFavorite />} />
+                        <Route element={<MainLayout/>}>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/musics" element={<Music/>}/>
+                            <Route path="/favorites" element={<MyFavorite/>}/>
 
                         </Route>
                     ) : null}
 
                     {/* Trang login không có Header */}
-                    <Route path="/login" element={<Login setAuth={setAuth} />} />
+                    <Route path="/login" element={<Login setAuth={setAuth}/>}/>
+                    <Route path="/register" element={<Register setAuth={setAuth}/>}/>
+                    {/* User Profile */}
+                    <Route path="/userProfile" element={<UserProfile />}/>
 
+                    {/* Music Player */}
                     {/* Admin Dashboard */}
-                    <Route path="/dashboard/*" element={<Dashboard />} />
+                    <Route path="/dashboard/*" element={<Dashboard/>}/>
                 </Routes>
 
                 {isSongPlaying && (
                     <motion.div
-                        initial={{ opacity: 0, y: 50 }}
-                        animate={{ opacity: 1, y: 0 }}
+                        initial={{opacity: 0, y: 50}}
+                        animate={{opacity: 1, y: 0}}
                         className="fixed min-w-[700px] h-26 inset-x-0 bottom-0 bg-cardOverlay drop-shadow-2xl backdrop-blur-md flex items-center justify-center"
                     >
-                        <MusicPlayer />
+                        <MusicPlayer/>
                     </motion.div>
                 )}
 
